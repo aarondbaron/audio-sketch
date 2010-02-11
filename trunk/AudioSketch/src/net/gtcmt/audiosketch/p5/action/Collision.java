27402e09568676efe20dd375e336b050b7dd4277
AaronAlbin <AaronAlbin@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-02-11 07:00:53 +0000

    still working on ClockBar for collision

--- trunk/AudioSketch/src/net/gtcmt/audiosketch/p5/action/Collision.java
+++ trunk/AudioSketch/src/net/gtcmt/audiosketch/p5/action/Collision.java
@@ -29,6 +29,80 @@ public class Collision {
 		}
 	}
 	
+	public static void collideClockBar(SoundObject soundObject, PlayBackBar playBar){
+		if(!soundObject.getCollideState(playBar)) {
+			
+
+			float[] iSectPoints = sphere_line_iSect(0, 0, 0,playBar.getPosX(), playBar.getPosY(), 0, 0, soundObject.getPosX(), soundObject.getPosY(), soundObject.getWidth());
+
+			
+			if (iSectPoints[0]>0) {
+				for(int i = 0; i<iSectPoints.length;i++){
+					System.out.println("iSectPoints[" + i + "]: "+ iSectPoints[i]);
+					
+				}
+				AudioControl.getAudioCtrl().trigger(soundObject.getSndType().toString(), (float) Math.random()*2);
+				//soundObject.play();
+				soundObject.setCollideState(playBar, true);
+				soundObject.setCollide(true);
+				soundObject.setGetFrame(true);
+				soundObject.setStartTime(System.currentTimeMillis());		
+			}
+		}
+	}
+	
+	/**
+	 * computes 2d/3d circle/sphere - line intersection points 
+	 * based on code by peter bourke 
+	 * returns array of floats, where first item specifies number of intersection points found {0, 1 or 2} 
+	 * set Z coordinates to 0 for 2D intersection checks 
+	 * line defined by points {x1;y1;z1} -> {x2;y2;z2} 
+	 * circle/sphere defined by center point at {x3;y3;z3} and radius r 
+	 **/
+	public static float[] sphere_line_iSect(int x1, int y1, int z1,int x2, int y2, int
+			z2,int x3, int y3, int z3, int r) {
+			 float[] points=new float[7];
+			 //float a =  sq(x2 - x1) + sq(y2 - y1) + sq(z2 - z1);
+			 float a =  (float) (Math.pow(x2-x1, 2) + Math.pow(y2-y1,2) + Math.pow(z2-z1,2));
+			 
+			 float b =  2* ( (x2 - x1)*(x1 - x3) + (y2 - y1)*(y1 - y3) + (z2 -
+			z1)*(z1 - z3) ) ;
+			 float c =  x3*x3 + y3*y3 + z3*z3 + x1*x1 + y1*y1 + z1*z1 - 2* (
+			x3*x1 + y3*y1 + z3*z1 ) - r*r ;
+			 float i = b * b - 4 * a * c;
+
+			 if ( i < 0.0 ) {
+			   // no intersection
+			   points[0] = 0;
+			   return(points);
+
+			 } else if ( i == 0.0 ) {
+			   // one intersection
+			   points[0] = 1;
+			   float mu = -b/(2*a) ;
+			   points[1] = x1 + mu*(x2-x1);
+			   points[2] = y1 + mu*(y2-y1);
+			   points[3] = z1 + mu*(z2-z1);
+			   return(points);
+
+			 } else {
+			   // two intersections
+			   points[0] = 2;
+			   // first intersection
+			   float mu = (float)(-b + Math.sqrt( b*b - 4*a*c )) / (2*a);
+			   points[1] = x1 + mu*(x2-x1);
+			   points[2] = y1 + mu*(y2-y1);
+			   points[3] = z1 + mu*(z2-z1);
+			   // second intersection
+			   mu = (float)(-b - Math.sqrt(b*b - 4*a*c )) / (2*a);
+			   points[4] = x1 + mu*(x2-x1);
+			   points[5] = y1 + mu*(y2-y1);
+			   points[6] = z1 + mu*(z2-z1);
+			   return(points);
+			   
+			 }
+	}
+	
 	/**
 	 * Detects collision for time line bar. When collision happens initiate action.
 	 */
