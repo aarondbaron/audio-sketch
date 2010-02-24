5bbc7b638a96cdc82d9e4242bf32eda84fc9010a
AaronAlbin <AaronAlbin@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-02-24 19:33:49 +0000

    colorbar change color

--- AudioSketch/src/net/gtcmt/audiosketch/p5/object/playbar/SquareBar.java
+++ AudioSketch/src/net/gtcmt/audiosketch/p5/object/playbar/SquareBar.java
@@ -5,6 +5,9 @@ import net.gtcmt.audiosketch.p5.util.P5Constants.PlayBackType;
 import processing.core.PApplet;
 
 public class SquareBar extends PlayBackBar {
+	
+	int strokeParam=0;
+	int strokeUpdate=1;
 
 	public SquareBar(P5Points2D objPos, float speed, float angle,
 			PlayBackType pbType, PApplet p) {
@@ -14,8 +17,20 @@ public class SquareBar extends PlayBackBar {
 
 	@Override
 	public void draw() {
+		strokeParam=strokeParam+strokeUpdate;
+		if(strokeParam==256)
+		{
+			strokeParam=255;
+			strokeUpdate=-1;
+		}
+		if(strokeParam==-1)
+		{
+			strokeParam=0;
+			strokeUpdate=1;
+		}
+		
 		p5.strokeWeight(10);
-		p5.stroke(255, 25, 255, 200);
+		p5.stroke(255, strokeParam, 255, 200);
 		p5.fill(0, 0, 0, 0);
 		p5.rect(playbarPos.getPosX(), playbarPos.getPosY(), playbarSize.getWidth(), playbarSize.getHeight());
 		playbarSize.setSize(((int) (playbarSize.getWidth()+speed)), ((int) (playbarSize.getHeight()+speed)));
