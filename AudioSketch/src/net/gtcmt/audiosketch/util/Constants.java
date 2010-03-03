6bc85f750cc137f7baab4920a557860e48268e78
AaronAlbin <AaronAlbin@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-03-03 00:11:17 +0000

    working on loading file names of audio

--- AudioSketch/src/net/gtcmt/audiosketch/util/Constants.java
+++ AudioSketch/src/net/gtcmt/audiosketch/util/Constants.java
@@ -1,6 +1,23 @@
 package net.gtcmt.audiosketch.util;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class Constants {
 
 	public static final String SOUND_OBJECT_PATH = "lib/image/";
+	public static final String SOUND_PATH = "lib/max_patches/";
+	
+	
+	//Sound files
+	public static File soundDir = new File(Constants.SOUND_OBJECT_PATH);
+	public static FilenameFilter filter2 = new FilenameFilter() {
+	    public boolean accept(File soundDir, String name) {
+	        return name.endsWith(".wav");
+	    }
+	};
+	public static String[] children2 = soundDir.list(filter2);		
+	public static String[] SOUND_NAME = children2;
+	public static int NUM_SOUNDS = children2.length;
+	
 }
