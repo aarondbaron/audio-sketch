19c366cf19c1b4aa7591462d38440f6c76440341
bpb54321 <bpb54321@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-03-09 07:01:24 +0000

    reactivision stuff.

--- AudioSketch/src/net/gtcmt/audiosketch/p5/object/TableMessageRouter.java
+++ AudioSketch/src/net/gtcmt/audiosketch/p5/object/TableMessageRouter.java
@@ -1,5 +1,6 @@
 package net.gtcmt.audiosketch.p5.object;
 
+import net.gtcmt.audiosketch.gui.util.GUIConstants;
 import net.gtcmt.audiosketch.p5.util.P5Points2D;
 import net.gtcmt.audiosketch.p5.util.P5Size2D;
 import net.gtcmt.audiosketch.p5.util.P5Constants.ObjectColorType;
@@ -12,48 +13,58 @@ public class TableMessageRouter {
 	private OscP5 oscP5;
 	private int id, xPos, yPos;
 	private MusicalWindow theMusicalWindow;
+	private final int firstMarkerIndex=114;
 	
-	public static final int tableMessageInPort=2000;
+	public static final int tableMessageInPort=2100;
 	
 	public TableMessageRouter(MusicalWindow theMusicalWindow) {
 		this.oscP5 = new OscP5(this, TableMessageRouter.tableMessageInPort);		
 		this.theMusicalWindow = theMusicalWindow;
-//		System.out.println("TableMessageRouter Initialized");
+		System.out.println("TableMessageRouter Initialized");
+		
+		//
+		xPos=300;
+		yPos=300;
+		//theMusicalWindow.addTableObject(3,5, ObjectColorType.WHITE, SndType.values()[id], new P5Points2D(xPos, yPos), new P5Size2D(70, 70), 64);
 	}
 	
 	/* incoming osc message are forwarded to the oscEvent method. */
+	
 	public void oscEvent(OscMessage theOscMessage) {
-//		System.out.println(theOscMessage.toString());
+		//System.out.println(theOscMessage.toString());
 		
 		if (theOscMessage.addrPattern().equalsIgnoreCase("/addObject")) {
-			id = theOscMessage.get(0).intValue();
-			xPos = theOscMessage.get(1).intValue();
-			yPos = theOscMessage.get(2).intValue();
+			id = offsetIndex(theOscMessage.get(0).intValue());
+			xPos = scalePos(theOscMessage.get(1).floatValue(),GUIConstants.WINDOW_WIDTH);
+			yPos = scalePos(theOscMessage.get(2).floatValue(),GUIConstants.WINDOW_HEIGHT);
 
-//			System.out.println("Add Object ID: " + id + " x: " + xPos + " y: " + yPos);
-			theMusicalWindow.addTableObject(id,id, ObjectColorType.ORANGE, SndType.BUZZ, new P5Points2D(xPos, yPos), new P5Size2D(30, 30), 64);
+			System.out.println("Add Object ID: " + id + " x: " + xPos + " y: " + yPos);
+			theMusicalWindow.addTableObject(id,id, ObjectColorType.WHITE, SndType.values()[id], new P5Points2D(xPos, yPos), new P5Size2D(70, 70), 64);
+			
+			
 
 			
 		} else if (theOscMessage.addrPattern().equalsIgnoreCase("/removeObject")) {
-			id = theOscMessage.get(0).intValue();	
+			id = offsetIndex(theOscMessage.get(0).intValue());	
 			
 			theMusicalWindow.removeTableObject(id);
 			
-//			System.out.println("Remove Object ID: " + id);
+			System.out.println("Remove Object ID: " + id);
 			
 		} else if (theOscMessage.addrPattern().equalsIgnoreCase("/updateObject")) {
-			id = theOscMessage.get(0).intValue();
-			xPos = theOscMessage.get(1).intValue();
-			yPos = theOscMessage.get(2).intValue();
+			id = offsetIndex(theOscMessage.get(0).intValue());
+			xPos = scalePos(theOscMessage.get(1).floatValue(),GUIConstants.WINDOW_WIDTH);
+			yPos = scalePos(theOscMessage.get(2).floatValue(),GUIConstants.WINDOW_HEIGHT);
 			
 			theMusicalWindow.moveObject(id, xPos, yPos);
 
-//			System.out.println("Position Object ID: " + id + " x: " + xPos + " y: " + yPos);
+			//System.out.println("Position Object ID: " + id + " x: " + xPos + " y: " + yPos);
 		}
 			
 			
 //		this.haveReceivedMsg=true;
 	}
+	
 	
 //	public int scaleDim(int windowDim, float posFloat,boolean flipped) {
 //		
@@ -65,5 +76,27 @@ public class TableMessageRouter {
 //		}
 //		
 //	}
+	
+	/**
+	 * @param index Actual index of fiducial marker.
+	 * @return The object indices shifted to start at 0.
+	 */
+	public int offsetIndex(int index) {
+		
+		return index-this.firstMarkerIndex;
+	}
+	
+	/**
+	 * 
+	 * @param floatPos position of the marker in float from 0-1.
+	 * @param windowDim The dimension of the window in integer format.
+	 * @return The scaled position.
+	 */
+	public int scalePos(float floatPos, int windowDim) {
+		
+		return (int)(floatPos*windowDim);
+		
+		
+	}
 }
 
