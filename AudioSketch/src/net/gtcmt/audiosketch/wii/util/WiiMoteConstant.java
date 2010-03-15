8d30b79aa2d53609fcdf27236db528403dc016e2
nav.reyort <nav.reyort@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-03-15 16:07:03 +0000

    jl one mote

--- AudioSketch/src/net/gtcmt/audiosketch/wii/util/WiiMoteConstant.java
+++ AudioSketch/src/net/gtcmt/audiosketch/wii/util/WiiMoteConstant.java
@@ -7,7 +7,7 @@ import java.util.Hashtable;
 public class WiiMoteConstant {
 
 	public static final float MAX_MOTE_IR_LENGTH = 1000f;
-	public static final String[] MOTE_MAC_ADDR = {"001BEA04CFBE", "001A19C0F036"};//,"00191D5367E7"};
+	public static final String[] MOTE_MAC_ADDR = {"00191D5367E7"};//"001BEA04CFBE"};//, "001A19C0F036"};
 	
 	public static java.util.Hashtable<String, int[]> wiiMoteCalData;
 	//Wii mote with address 00191D5367E7 
@@ -16,6 +16,6 @@ public class WiiMoteConstant {
 	public static void initCalData(){
 		wiiMoteCalData =  new Hashtable<String, int[]>();
 		wiiMoteCalData.put(MOTE_MAC_ADDR[0], eSevenCalibration);
-		wiiMoteCalData.put(MOTE_MAC_ADDR[1], eSevenCalibration);
+		//wiiMoteCalData.put(MOTE_MAC_ADDR[1], eSevenCalibration);
 	}
 }
