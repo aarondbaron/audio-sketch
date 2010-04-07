3c6b1ce5d58a24879469153a6d3f246201498162
AaronAlbin <AaronAlbin@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-04-07 19:50:33 +0000

    adding SquareFillBar

--- AudioSketch/src/net/gtcmt/audiosketch/p5/object/playbar/PlayBackBar.java
+++ AudioSketch/src/net/gtcmt/audiosketch/p5/object/playbar/PlayBackBar.java
@@ -99,7 +99,8 @@ public abstract class PlayBackBar {
 		{
 		case RADIAL:		return new RadialBar(mousePnt, speed, angle, barType, p5);
 		case RADIAL2: 	return new Radial2Bar(mousePnt, speed, angle, barType, p5); 
-		case CIRCLEFILLBAR: 	return new CircleFillBar(mousePnt, speed, angle, barType, p5); 	
+		case CIRCLEFILLBAR: 	return new CircleFillBar(mousePnt, speed, angle, barType, p5); 
+		case SQUAREFILLBAR: 	return new SquareFillBar(mousePnt, speed, angle, barType, p5); 	
 		case SQUAREBAR: 	return new SquareBar(mousePnt, speed, angle, barType, p5);
 		case CLOCKBAR:	return new ClockBar(mousePnt, speed, angle, barType, p5);	
 		case BAR:		return new Bar(mousePnt, speed, angle, barType, p5);	
