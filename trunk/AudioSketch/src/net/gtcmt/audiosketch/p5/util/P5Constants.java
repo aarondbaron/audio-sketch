60bdae54e5bb0dcc25f3f073cb43253412b981d6
AaronAlbin <AaronAlbin@8468eb6c-0c55-11df-93c6-ad3ce60bb317>
2010-04-07 20:56:09 +0000

    hopefully things fixed

--- trunk/AudioSketch/src/net/gtcmt/audiosketch/p5/util/P5Constants.java
+++ trunk/AudioSketch/src/net/gtcmt/audiosketch/p5/util/P5Constants.java
@@ -9,10 +9,10 @@ public class P5Constants {
 
 	//Playback bar 
 	public enum PlayBackType{
-		BAR, RADIAL, RADIAL2, BAR2, CLOCKBAR, SQUAREBAR, CIRCLEFILLBAR, SQUAREFILLBAR
+		BAR, RADIAL, RADIAL2, BAR2, CLOCKBAR, SQUAREBAR, SQUAREBAR2, CIRCLEFILLBAR, SQUAREFILLBAR
 	}
 	
-	public static final String[] BAR_TYPE = {"Bar", "Radial", "Radial2" ,"Bar2", "ClockBar","SquareBar","CircleFillBar","SquareFillBar"};
+	public static final String[] BAR_TYPE = {"Bar", "Radial", "Radial2" ,"Bar2", "ClockBar","SquareBar","SquareBar2","CircleFillBar","SquareFillBar"};
 	public static final int MIN_SPEED=1;
 	public static final int MAX_SPEED=11;
 	public static final float MAX_TRIG_DISTANCE = 20;
